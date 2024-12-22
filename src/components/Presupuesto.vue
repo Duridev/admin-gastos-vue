db5ddb66e951e97096cb2f8754bdebad86aabb92
duridev <duridev@outlook.com>
2024-12-22 19:48:22 -0300

    Finalizacion formulario de presupuesto

--- src/components/Presupuesto.vue
+++ src/components/Presupuesto.vue
@@ -4,7 +4,7 @@
 
 
 <template>
-    <form>
+    <form class="presupuesto">
         <div class="campo">
             <label for="nuevo-presupuesto">Definir Presupuesto</label>
 
@@ -12,6 +12,7 @@
                 id="nuevo-presupuesto"
                 class="nuevo-presupuesto"
                 placeholder="Añade tu presupuesto"
+                type="number"
             />
 
         </div>
@@ -23,5 +24,49 @@
 
 
 <style scoped>
+
+.presupuesto {
+    width: 100%;
+}
+
+.campo {
+    display: grid;
+    gap: 2rem;
+}
+
+.presupuesto label {
+    font-size: 2.2rem;
+    text-align: center;
+    color: var(--azul);
+}
+
+.presupuesto input[type="number"] {
+    background-color: var(--gris-claro);
+    border-radius: 1rem;
+    padding: 1rem;
+    border: none;
+    font-size: 2.2rem;
+    text-align: center;
+}
+
+.presupuesto input[type="submit"] {
+    background-color: var(--azul);
+    border-radius: 1rem;
+    padding: 1rem;
+    border: none;
+    font-size: 2rem;
+    text-align: center;
+    margin-top: 2rem;
+    color: var(--blanco);
+    font-weight: 900;
+    width: 100%;
+    transition: background-color 300ms ease;
+}
+
+.presupuesto input[type="submit"]:hover {
+    cursor: pointer;
+    background-color: #1048a4;
+}
+
 
 </style>
