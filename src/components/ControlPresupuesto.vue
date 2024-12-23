93542848ede2ba98d75587dde1a825d65f8bf580
duridev <duridev@outlook.com>
2024-12-22 22:55:55 -0300

    CSS al panel de presupuesto

--- src/components/ControlPresupuesto.vue
+++ src/components/ControlPresupuesto.vue
@@ -1,20 +1,18 @@
 <script setup>
-    import imagen from '../assets/img/grafico.jpg'
+import imagen from '../assets/img/grafico.jpg'
 
 </script>
 
 
 <template>
-    <div class="dons-columnas">
+    <div class="dos-columnas">
 
         <div class="contenedor-grafico">
             <img :src="imagen" alt="">
         </div>
 
         <div class="contenedor-presupuesto">
-            <button
-            class="reset-app"
-            >
+            <button class="reset-app">
                 Resetear App
             </button>
 
@@ -37,5 +35,62 @@
 
 
 <style scoped>
+    .dos-columnas {
+        display: flex;
+        flex-direction: column;
+    }
 
+    .dos-columnas> :first-child {
+        margin-bottom: 3rem;
+    }
+
+
+    .contenedor-presupuesto {
+        width: 100%;
+    }
+
+    .contenedor-presupuesto p {
+        font-size: 2.4rem;
+        text-align: center;
+        color: var(--gris-oscuro);
+    }
+
+    .contenedor-presupuesto span {
+        font-weight: 900;
+        color: var(--azul);
+    }
+
+    .reset-app {
+        background-color: #db2777;
+        border: none;
+        padding: 1rem;
+        width: 100%;
+        color: var(--blanco);
+        font-weight: 900;
+        text-transform: uppercase;
+        border-radius: 1rem;
+        transition: background-color 300ms ease;
+    }
+
+    .reset-app:hover {
+        cursor: pointer;
+        background-color: #c11d67;
+    }
+
+
+    @media (min-width: 768px) {
+        .dos-columnas {
+            flex-direction: row;
+            gap: 4rem;
+            align-items: center;
+        }
+
+        .dos-columnas> :first-child {
+            margin-bottom: 0;
+        }
+
+        .contenedor-presupuesto p {
+        text-align: left;
+        }
+    }
 </style>
